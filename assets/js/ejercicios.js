764945bdc7496256dc2df0686a501f2515992f3e
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-16 21:06:36 -0300

    Ejercicios

---
+++ assets/js/ejercicios.js
@@ -0,0 +1,69 @@
+// Ejercicios de Interpolación de Variables
+// 1. Ejercicio: Saludo personalizado
+// Consigna: Crea una variable nombre con tu nombre y usa interpolación de variables para imprimir un saludo como "Hola, mi nombre es [nombre]".
+
+
+//------------------------------------------
+// 2. Ejercicio: Descripción del objeto
+// Consigna: Crea variables producto, precio y cantidad, y usa interpolación de variables para imprimir "El producto [producto] cuesta [precio] dólares y tienes [cantidad] unidades".
+
+
+
+//------------------------------------------
+
+// 3. Ejercicio: Operaciones matemáticas
+// Consigna: Crea dos variables a y b con valores numéricos e imprime la suma de ambas en una frase como "La suma de [a] y [b] es [resultado]".
+
+
+//---------------------------------
+
+
+// Ejercicios sobre el Uso Global y en Bloque de var y let
+// 4. Ejercicio: var en ámbito global
+// Consigna: Declara una variable var fuera de una función, luego vuelve a declararla dentro de la función y muestra su valor dentro y fuera de la función.
+
+// 
+
+//---------------------------------
+
+// 5. Ejercicio: let en ámbito de bloque
+// Consigna: Declara una variable let dentro de un bloque if y luego intenta acceder a esa variable fuera del bloque. (deberá arrojar error)
+
+
+
+//---------------------------------
+
+//6.Ejercicio: Reasignación de var
+// Consigna: Declara una variable var, reasígnale un valor dentro de una función y luego imprime el valor fuera de la función.
+
+
+
+//---------------------------------
+
+//7.  Ejercicio: Reasignación de let en bloque
+// Consigna: Declara una variable let dentro de un bloque y reasígnale un valor en otro bloque, mostrando que son variables diferentes.
+
+
+
+//---------------------------------
+
+// Ejercicios de Destructuring con Objetos y Arrays
+// 8. Ejercicio: Destructuring básico de objetos
+// Consigna: Usa destructuring para extraer las propiedades nombre y edad de un objeto persona y mostrarlas en consola.
+
+
+//---------------------------------
+
+
+//9. Ejercicio: Destructuring con alias en objetos
+// Consigna: Usa destructuring para extraer la propiedad modelo de un objeto coche y renombrarla como carModel.
+
+
+//---------------------------------
+
+
+//10. Ejercicio: Destructuring de arrays
+// Consigna: Usa destructuring para extraer los dos ultimos elementos de este array: const frutas = ["Manzana", "Banana", "Naranja"];
+
+
+//---------------------------------
